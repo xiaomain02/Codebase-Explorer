4985ed88e94b3cd2e3650d9a3e71d6c679cb822a
ksinuss <kostionovaki@gmail.com>
2026-03-22 19:52:58 +0300

    testing api for personal understanding

---
+++ tests/test_upload.py
@@ -0,0 +1,16 @@
+import requests
+
+with open('repo_for_test_upload.zip', 'rb') as f:
+    response = requests.post('http://localhost:8000/repo/upload', files={'file': f})
+
+repo_info = response.json()
+print(repo_info)
+
+repo_tree = requests.get(f'http://localhost:8000/repo/{repo_info['repo_id']}/tree')
+print(repo_tree.json())
+
+repo_modules = requests.get(f'http://localhost:8000/repo/{repo_info['repo_id']}/modules')
+print(repo_modules.json())
+
+repo_summary = requests.get(f'http://localhost:8000/repo/{repo_info['repo_id']}/summary')
+print(repo_summary.json())
